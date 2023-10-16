1889cf24821c39f4bcedb6e1f3a7b9a0ea747b33
Nur Mohammed Imtiaz <76715694+fimtiaz4@users.noreply.github.com>
2023-10-16 07:58:22 +0600

    Delete src directory

--- src/store/user.js
+++
@@ -1,9 +0,0 @@
-const localData = JSON.parse(localStorage.getItem("user"));
-// import { reactive, computed } from "vue";
-// const UData = reactive({
-//   Uitem: {},
-//   saveUserInLocalStorage() {
-//     localStorage.setItem("user", JSON.stringify(this.Uitem));
-//   },
-// });
-export { localData };
